c43961ac883294a64bfd455cf3bf7d8b6624cf24
albahagiadp <109083257+albahagiadp@users.noreply.github.com>
2022-07-12 09:58:12 +0700

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -60,63 +60,6 @@ const skillsSection = {
   /* Make Sure to include correct Font Awesome Classname to view your icon
 https://fontawesome.com/icons?d=gallery */
 
-  softwareSkills: [
-    {
-      skillName: "html-5",
-      fontAwesomeClassname: "fab fa-html5"
-    },
-    {
-      skillName: "css3",
-      fontAwesomeClassname: "fab fa-css3-alt"
-    },
-    {
-      skillName: "sass",
-      fontAwesomeClassname: "fab fa-sass"
-    },
-    {
-      skillName: "JavaScript",
-      fontAwesomeClassname: "fab fa-js"
-    },
-    {
-      skillName: "reactjs",
-      fontAwesomeClassname: "fab fa-react"
-    },
-    {
-      skillName: "nodejs",
-      fontAwesomeClassname: "fab fa-node"
-    },
-    {
-      skillName: "swift",
-      fontAwesomeClassname: "fab fa-swift"
-    },
-    {
-      skillName: "npm",
-      fontAwesomeClassname: "fab fa-npm"
-    },
-    {
-      skillName: "sql-database",
-      fontAwesomeClassname: "fas fa-database"
-    },
-    {
-      skillName: "aws",
-      fontAwesomeClassname: "fab fa-aws"
-    },
-    {
-      skillName: "firebase",
-      fontAwesomeClassname: "fas fa-fire"
-    },
-    {
-      skillName: "python",
-      fontAwesomeClassname: "fab fa-python"
-    },
-    {
-      skillName: "docker",
-      fontAwesomeClassname: "fab fa-docker"
-    }
-  ],
-  display: true // Set false to hide this section, defaults to true
-};
-
 // Education Section
 
 const educationInfo = {
@@ -124,7 +67,7 @@ const educationInfo = {
   schools: [
     {
       schoolName: "University of Brawijaya",
-      logo: require("./assets/images/harvardLogo.png"),
+      logo: require("./assets/images/brawijayaLogo.jpg"),
       subHeader: "Bachelor Degree of Engineering in Electrical Engineering",
       duration: "August 2017 - June 2021",
       desc: "Participated in the research of XXX and published 3 papers.",
